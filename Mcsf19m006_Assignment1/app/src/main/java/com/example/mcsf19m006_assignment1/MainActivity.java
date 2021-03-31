9fe0f3313ca68fa844d4c406974e78b82eeb3fa1
jawaria6 <mcsf19m006@pucit.edu.pk>
2021-03-31 18:03:55 +0500

    Assignment1

---
+++ Mcsf19m006_Assignment1/app/src/main/java/com/example/mcsf19m006_assignment1/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.mcsf19m006_assignment1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView=findViewById(R.id.v);
+        imageView.setImageResource(R.drawable.kids);
+
+    }
+
+    public void Move(View view) {
+        Intent intent=new Intent(this,EngAlphabets.class);
+        startActivity(intent);
+    }
+}
